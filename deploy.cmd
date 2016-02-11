5aabda8373356ac33cbd86a680e60556230540bf
Nabin Karki Thapa <nabin@outlook.com>
2016-02-11 14:43:00 +1100

    Update deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -87,6 +87,7 @@ set DNX_RUNTIME="%USERPROFILE%\.dnx\runtimes\dnx-clr-win-x86.1.0.0-rc1-final"
 
 :: 2. Install DNX
 call :ExecuteCmd PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';$CmdPathFile='%DNVM_CMD_PATH_FILE%';& '%SCM_DNVM_PS_PATH%' " install 1.0.0-rc1-final -arch x86 -r clr %SCM_DNVM_INSTALL_OPTIONS%
+echo !ERRORLEVEL!
 IF !ERRORLEVEL! NEQ 0 goto error
 
 
